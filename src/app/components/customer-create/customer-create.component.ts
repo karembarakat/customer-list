3844f21970010e0354a6068c0cc17d0eadfc30bb
devkarem <karemmahmed79@gmail.com>
2025-01-04 10:50:48 +0200

    Linked Backend With frontend

--- src/app/components/customer-create/customer-create.component.ts
+++ src/app/components/customer-create/customer-create.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-customer-create',
-  imports: [],
+  imports: [
+    MatButtonModule,
+    RouterModule
+  ],
   templateUrl: './customer-create.component.html',
   styleUrl: './customer-create.component.css'
 })
--- src/app/components/customer-list/customer-list.component.ts
+++ src/app/components/customer-list/customer-list.component.ts
@@ -1,13 +1,43 @@
-import { Component } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
+import { Component, inject, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule, provideRouter } from '@angular/router';
+import { CustomerService } from '../../services/customer.service';
+import { log } from 'console';
+import { Customer } from '../../models/customer.model';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-customer-list',
-  imports: [MatButtonModule],
+  imports: [MatButtonModule,
+    RouterModule,
+    CommonModule,
+    MatIconModule
+  ],
   templateUrl: './customer-list.component.html',
   styleUrl: './customer-list.component.css'
 })
-export class CustomerListComponent {
+export class CustomerListComponent implements OnInit {
+
+  // Inject Customer Service
+  private customerService = inject(CustomerService);
+
+  customers!: Customer[];
+  ngOnInit(): void {
+    this.customerService.get().subscribe(
+      data => {
+        console.log('Customer:', data);
+        this.customers = data
+      },
+      error => {
+        console.error('Error:', error);
+      }
+    );
+  }
+  onDeleteClick(customer: Customer) {
+    throw new Error('Method not implemented.');
+  }
+
 
 }
