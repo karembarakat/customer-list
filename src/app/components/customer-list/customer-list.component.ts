292639561952ea633a2f22e8210d60108cfcd5bf
devkarem <karemmahmed79@gmail.com>
2025-01-04 09:56:02 +0200

    Created Backend, added ng UI , added tailwindcss, created main routs

---
+++ src/app/components/customer-list/customer-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+
+
+@Component({
+  selector: 'app-customer-list',
+  imports: [MatButtonModule],
+  templateUrl: './customer-list.component.html',
+  styleUrl: './customer-list.component.css'
+})
+export class CustomerListComponent {
+
+}
